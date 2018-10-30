6d84e4ce80dccff9bd04d2ef494e3423cd068ff7
Zhanadil Nurtoleuov <znurtoleuov@gmail.com>
2018-10-30 16:25:55 +0600

    application object moved to class

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ require('module-alias/register');
 
 const app = require('@root/app');
 
-const port = process.env.PORT || 3000;
-app.listen(port);
+app.server.listen(app.port);
--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const Express = require('express');
 const morgan = require('morgan');
 const body_parser = require('body-parser');
 const mongoose = require('mongoose');
@@ -14,77 +14,90 @@ const logger = require('@root/logger');
 const router = require('@routes');
 const mailer = require('@lib/mailer');
 
-const app = express();
-const server = http.createServer(app);
+const applySockets = require('@root/socket');
 
-// Инициализируем сокетный сервер
-require('@root/socket')(server);
+class App {
+    constructor() {
+        let JWT_SECRET = this.JWT_SECRET = config.get('JWT_SECRET');
+        this.env = config.util.getEnv('NODE_ENV');
+        this.port = config.util.getEnv('PORT') || 3000;
 
-const JWT_SECRET = config.get('JWT_SECRET');
+        let express = this.express = Express();
+        this.server = http.createServer(express);
 
-mailer.init(
-    'znurtoleuov@gmail.com',
-    '3.3&d6Q,oL'
-);
+        // Инициализируем сокетный сервер
+        applySockets(this.server);
 
-mongoose.Promise = global.Promise;
-let connectionOptions = {
-    auth: {
-        authSource: "admin"
-    },
-    useNewUrlParser: true
-};
-if (config.util.getEnv('NODE_ENV') !== 'production') {
-    connectionOptions.auth = undefined;
-}
-mongoose.connect(
-    config.DBHost,
-    connectionOptions,
-    function(err, db){
-        if(err){
-            console.log(`${err.message}`);
-            //logger.emerg(`mongodb error: ${err.message}`);
-        } else {
-            logger.info('mongodb successfully started');
+        mailer.init(
+            'znurtoleuov@gmail.com',
+            '3.3&d6Q,oL'
+        );
+
+        // Подключаем базу данных
+        mongoose.Promise = global.Promise;
+        let connectionOptions = {
+            auth: {
+                authSource: "admin"
+            },
+            useNewUrlParser: true
+        };
+        if (this.env !== 'production') {
+            connectionOptions.auth = undefined;
         }
-    }
-)
-
-app.use(body_parser.json());
-app.use(fileUpload());
-app.use(cors());
-
-// Log all requests
-app.use((req, res, next) => {
-    // If no token received
-    if (req.headers.authorization === undefined) {
-        logger.info(req.url, {info: "no token"})
-        next();
-    } else {
-        // If token is received, then decode
-        JWT.verify(req.headers.authorization, JWT_SECRET, (err, decoded) => {
-            if (err) {
-                logger.info(req.url, {info: "incorrect token"});
-            } else {
-                // If token is correct, then log credentials
-                logger.info(req.url, {sub: decoded.sub});
+        mongoose.connect(
+            config.DBHost,
+            connectionOptions,
+            function(err, db){
+                if(err){
+                    console.log(`${err.message}`);
+                    //logger.emerg(`mongodb error: ${err.message}`);
+                } else {
+                    logger.info('mongodb successfully started');
+                }
             }
-            next();
+        )
+
+        // Подключаем нужные нам миддлы
+        express.use(body_parser.json());
+        express.use(fileUpload());
+        express.use(cors());
+
+        // Подключаем логгер
+        express.use((req, res, next) => {
+            // If no token received
+            if (req.headers.authorization === undefined) {
+                logger.info(req.url, {info: "no token"})
+                next();
+            } else {
+                // If token is received, then decode
+                JWT.verify(req.headers.authorization, JWT_SECRET, (err, decoded) => {
+                    if (err) {
+                        logger.info(req.url, {info: "incorrect token"});
+                    } else {
+                        // If token is correct, then log credentials
+                        logger.info(req.url, {sub: decoded.sub});
+                    }
+                    next();
+                });
+            }
+        });
+
+        // Подключаем роутеры
+        express.use('/', router);
+
+        // Обработка 404
+        express.use((req, res, next) => {
+            return res.status(404).send('sorry, page not found');
+        });
+
+        // Обработка ошибок
+        express.use((err, req, res, next) => {
+            // TODO: log this.
+            return res.status(err.status || 500).json({
+                error: err.message
+            });
         });
     }
-});
+}
 
-app.use('/', router);
-
-app.use((req, res, next) => {
-    return res.status(404).send('sorry, page not found');
-});
-
-app.use((err, req, res, next) => {
-    // TODO: log this.
-    return res.status(err.status || 500).json({
-        error: err.message
-    });
-});
-
-module.exports = server;
+module.exports = new App();
